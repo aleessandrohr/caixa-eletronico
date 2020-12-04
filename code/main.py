139543b13e1437169f84d9c00200ba4e7ea59d15
Alessandro Ramos <toprime13@gmail.com>
2020-12-04 20:43:15 -0300

    resolvido bugs

--- code/main.py
+++ code/main.py
@@ -31,9 +31,9 @@ def menu(tamcédula_100, tamcédula_50, tamcédula_20, tamcédula_10):
             print('-' * 42)
             while True:
                 valor = int(input('Quanto deseja sacar? '))
-                if valor <= 1000:
+                if valor <= 3000:
                     break
-                print('Limite de R$1000 reais.')
+                print('Limite de R$3000 reais.')
             print('Executando a operação.')
             sleep(0.5)
             print('Contando as notas.')
@@ -53,34 +53,8 @@ def menu(tamcédula_100, tamcédula_50, tamcédula_20, tamcédula_10):
                                 tamcédula_100 -= 1
                                 tamcédula_100_cont += 1
                             else:
-                                tamcédula_100 += tamcédula_100_cont
                                 cédula = 50
-                        elif total >= 50:
-                            if tamcédula_50 != 0:
-                                total -= 50
-                                tamcédula_50 -= 1
-                                tamcédula_50_cont += 1
-                            else:
-                                cédula = 20
-                        elif total >= 20:
-                            if tamcédula_20 != 0:
-                                total -= 20
-                                tamcédula_20 -= 1
-                                tamcédula_20_cont += 1
-                            else:
-                                cédula = 10
-                        elif total >= 10:
-                            if tamcédula_10 != 0:
-                                total -= 10
-                                tamcédula_10 -= 1
-                                tamcédula_10_cont += 1
-                            else:
-                                print('Não há notas o suficiente')
-                                tamcédula_50 += tamcédula_50_cont
-                                tamcédula_20 += tamcédula_20_cont
-                                tamcédula_10 += tamcédula_10_cont
-                                tamcédula_100 += tamcédula_100_cont
-                                break
+                        else: cédula = 50
                     elif cédula == 50:
                         if total >= 50:
                             if tamcédula_50 != 0:
@@ -88,23 +62,9 @@ def menu(tamcédula_100, tamcédula_50, tamcédula_20, tamcédula_10):
                                 tamcédula_50 -= 1
                                 tamcédula_50_cont += 1
                             else:
-                                tamcédula_50 += tamcédula_50_cont
                                 cédula = 20
-                        elif total >= 20:
-                            if tamcédula_20 != 0:
-                                total -= 20
-                                tamcédula_20 -= 1
-                                tamcédula_20_cont += 1
-                            else:
-                                cédula = 10
-                        elif total >= 10:
-                            if tamcédula_10 != 0:
-                                total -= 10
-                                tamcédula_10 -= 1
-                                tamcédula_10_cont += 1
-                            else:
-                                print('Não há notas o suficiente')
-                                break
+                        else:
+                            cédula = 20
                     elif cédula == 20:
                         if total >= 20:
                             if tamcédula_20 != 0:
@@ -113,33 +73,27 @@ def menu(tamcédula_100, tamcédula_50, tamcédula_20, tamcédula_10):
                                 tamcédula_20_cont += 1
                             else:
                                 cédula = 10
-                        elif total >= 10:
-                            if tamcédula_10 != 0:
-                                total -= 10
-                                tamcédula_10 -= 1
-                                tamcédula_10_cont += 1
-                            else:
-                                print('Não há notas o suficiente')
-                                break
+                        else:
+                            cédula = 10
                     elif cédula == 10:
                         if total >= 10:
                             if tamcédula_10 != 0:
-                                total -= 10
-                                tamcédula_10 -= 1
-                                tamcédula_10_cont += 1
+                                    total -= 10
+                                    tamcédula_10 -= 1
+                                    tamcédula_10_cont += 1
                             else:
-                                print('Não há notas o suficiente')
+                                print('Não há notas o suficiente.')
+                                tamcédula_100 += tamcédula_100_cont
                                 tamcédula_50 += tamcédula_50_cont
                                 tamcédula_20 += tamcédula_20_cont
                                 tamcédula_10 += tamcédula_10_cont
-                                tamcédula_100 += tamcédula_100_cont
                                 break
                     if total == 0:
                         if tamcédula_100_cont != 0:
                             print(f'Retirado {tamcédula_100_cont} cédulas de 100 reais.')
                         if tamcédula_50_cont != 0:
                             print(f'Retirado {tamcédula_50_cont} cédulas de 50 reais.')
-                        if tamcédula_10_cont != 0:
+                        if tamcédula_20_cont != 0:
                             print(f'Retirado {tamcédula_20_cont} cédulas de 20 reais.')
                         if tamcédula_10_cont != 0:
                             print(f'Retirado {tamcédula_10_cont} cédulas de 10 reais.')
@@ -153,10 +107,10 @@ def menu(tamcédula_100, tamcédula_50, tamcédula_20, tamcédula_10):
 
 # Notas
 
-tamcédula_100 = 10
-tamcédula_50 = 20
-tamcédula_20 = 20
-tamcédula_10 = 100
+tamcédula_100 = 1
+tamcédula_50 = 1
+tamcédula_20 = 1
+tamcédula_10 = 1
 
 # Iniciar
 
